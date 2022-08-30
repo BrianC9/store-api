4086e03392133ba180b00b06e469f4f3d4f9ece3
Bryan Octavio <100432689@alumnos.uc3m.es>
2022-08-30 12:08:25 +0200

    create the product schema for mongo

---
+++ models/product.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Product name must be provided'],
+  },
+  price: {
+    type: Number,
+    required: [true, 'Price must be provided'],
+  },
+  featured: {
+    type: Boolean,
+    default: false,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  company: {
+    type: String,
+    enum: {
+      values: ['Ikea', 'Tien21', 'Lidl', 'Conforama'],
+      message: '{VALUE} is not a valid option',
+    },
+  },
+});
+const productModel = mongoose.model('product', productSchema);
+export default productModel;
